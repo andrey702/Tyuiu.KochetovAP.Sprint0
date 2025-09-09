6b05db83e04bb0d2c1120e4d43a0a1c4fc087621
andreykochetov <oliverr223344@gmail.com>
2025-09-09 16:27:12 +0600

    Task1

---
+++ Tyuiu.KochetovAP.Sprint1.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KochetovAP.Sprint1.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,World");
+            String name = "Андрей";
+            int age = 18;
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
