aed043da7fc2d33f23375a7016941937124f670f
andreykochetov <oliverr223344@gmail.com>
2025-09-12 18:48:34 +0600

    Task5

--- Tyuiu.KochetovAP.Sprint0.Task5.V0.Lib/DataService.cs
+++ Tyuiu.KochetovAP.Sprint0.Task5.V0.Lib/DataService.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tyuiu.KochetovAP.Sprint0.Task4.V0.Lib
+namespace Tyuiu.KochetovAP.Sprint0.Task5.V0.Lib
 {
     public class DataService
     {
+
         public static int Addition(int a, int b)
         {
             return a + b;
@@ -23,9 +25,19 @@ namespace Tyuiu.KochetovAP.Sprint0.Task4.V0.Lib
             return a * b;
         }
 
+
         public static double Division(int a, int b)
         {
-            return a / b;
+
+            if (b == 0)
+            {
+                Console.WriteLine("Переменная b = {0} на ноль делить нельзя", b);
+                return -1;
+            }
+            else
+            {
+                return (double)a / b;
+            }
         }
     }
 }
---
+++ Tyuiu.KochetovAP.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KochetovAP.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KochetovAP.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a686641-ad83-4737-82d1-174d35d30002")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
