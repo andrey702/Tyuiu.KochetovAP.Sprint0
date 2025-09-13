ef7fa869f7264bd77823fb5a2275907014706fc2
andreykochetov <oliverr223344@gmail.com>
2025-09-13 12:54:34 +0600

    Task7

---
+++ Tyuiu.KochetovAP.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using Tyuiu.KochetovAP.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.KochetovAP.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArraysValid()
+        {
+            int[] nums1 = new int[] { 1, 1, 1, 1, 1 };
+            int[] nums2 = new int[] { 1, 1, 1, 1, 1 };
+            int[] nums3 = new int[] { 2, 2, 2, 2, 2, };
+
+            int[] res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEqual(nums3, res);
+
+        }
+    }
+
+}
---
+++ Tyuiu.KochetovAP.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KochetovAP.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KochetovAP.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a02fa4aa-75e9-4f0d-be23-53202fd6be8b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
